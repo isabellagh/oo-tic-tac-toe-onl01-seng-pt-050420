9fc4d92be325c8bbed8624ebec9ed765dbea5e1b
isabella g henriques <isabellagh88@gmail.com>
2020-05-30 00:39:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -66,9 +66,9 @@ end
     puts "Please enter 1-9:"
     input = gets.strip
     index = input_to_index(input)
-    char = current_player
+    token = current_player
     if valid_move?(index)
-      move(index, char)
+      move(index, token)
       display_board
     else
       turn
